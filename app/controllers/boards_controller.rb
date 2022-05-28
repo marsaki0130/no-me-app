655b507280d39599c4823b0d3be3a902d4700b30
正木啓介 <kettan0130@gmail.com>
2022-05-28 10:11:44 +0900

    form_withで投稿画面作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -4,5 +4,21 @@ class BoardsController < ApplicationController
   end
 
   def new
+    @board = current_user.boards.build
   end
+
+  def create
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to boards_path, notice: '保存できました'
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def board_params
+      params.require(:board).permit(:title, :content)
+    end
 end
